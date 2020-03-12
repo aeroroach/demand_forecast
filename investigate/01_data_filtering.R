69e3d89a3cbba378845f490ce30338a5e5713f61
aeroroach <silencepill@gmail.com>
2020-03-12 10:46:57 +0700

    Initialize investigate script.

---
+++ investigate/01_data_filtering.R
@@ -0,0 +1,64 @@
+library(tidyverse)
+library(lubridate)
+
+# Prerequisite ------------------------------------------------------------
+
+source("1_data_cleansing.R")
+source("2_data_prep.R")
+
+script_path <- "D:/R_project/Demand_Forecast/"
+file_path <- "D:/R_project/Demand_Forecast/input/"
+con_name <- "latest_control.dat"
+dt_name <- "latest_TDM_HS.dat"
+map_name <- "model_mapping.dat"
+log_path <- "D:/R_project/Demand_Forecast/log/"
+out_path <- "D:/R_project/Demand_Forecast/output/"
+
+# Data Reading ------------------------------------------------------------
+
+
+full_dt <- data_clean(file_path, con_name, dt_name)
+
+fil_model <- c("A520-64CDMSL", "NOVA5T", "Y6SSL", "Y11CDMSL")
+
+full_dt %>% 
+  filter(trade_product_model %in% fil_model) -> full_dt
+
+trade_agg <- data_prep(full_dt, log_path)
+
+lambda <- read_delim("output/lambda_20200308_005621.csv", delim = "|")
+
+lambda %>% 
+  filter(trade_product_model %in% fil_model) %>% 
+  group_by(trade_product_brand, trade_product_model, product_subtype, age_week,
+           dis_cat, boosting_flag) %>% 
+  summarise(sum_pred = sum(pred)) %>% 
+  ungroup() %>% 
+  mutate(dis_cat = factor(dis_cat), 
+         boosting_flag = factor(boosting_flag)) -> lambda
+
+# Joining data ------------------------------------------------------------
+
+trade_agg %>% 
+  left_join(lambda,by = c("trade_product_brand", "trade_product_model", "product_subtype", 
+                          "age_week", "dis_cat", "boosting_flag")) %>% 
+  filter(boosting_flag == 0) -> trade_agg
+
+
+# Aggregate ---------------------------------------------------------------
+
+trade_agg %>% 
+  group_by(trade_product_brand, trade_product_model, product_subtype, 
+           age_week) %>% 
+  summarise(sales_week = sum(sales)) -> trade_sum
+
+trade_agg %>% 
+  group_by(trade_product_brand, trade_product_model, product_subtype, 
+           age_week) %>% 
+  summarise(pred_week = sum(sum_pred)) -> pred_sum
+
+trade_sum %>% 
+  left_join(pred_sum,by = c("trade_product_brand", "trade_product_model", "product_subtype", 
+                          "age_week")) %>% 
+  arrange(trade_product_brand, trade_product_model, product_subtype, age_week) -> trade_sum_compare
+
---
+++ investigate/log_reader.R
@@ -0,0 +1,59 @@
+library(tidyverse)
+
+lambda_tbl <- read_delim("output/lambda_20200311_141633.csv", delim = "|")
+
+omit <- read_csv("/data01/pitchaym/HS_forecast/log/omit_HS.csv")
+
+short <- read_csv("/data01/pitchaym/HS_forecast/production/log_short.csv")
+
+pre <- read_csv("/data01/pitchaym/HS_forecast/production/pre_Diag.csv")
+
+
+# Log comparison ----------------------------------------------------------
+
+
+log <- read_csv("log/log_ML_all.csv")
+
+log_2m <- read_csv("log/log_ML_2m.csv")
+
+log_3m <- read_csv("log/log_ML_3m.csv")
+
+log %>% 
+  select(HS_brand, HS_model, HS_subtype, RMSE, MAPE) %>% 
+  left_join(select(log_2m, HS_brand, HS_model, HS_subtype, RMSE_2m = RMSE, MAPE_2m = MAPE),
+            by = c("HS_brand", "HS_model", "HS_subtype")) %>% 
+  left_join(select(log_3m, HS_brand, HS_model, HS_subtype, RMSE_3m = RMSE, MAPE_3m = MAPE),
+            by = c("HS_brand", "HS_model", "HS_subtype")) -> log_compare
+
+# Calculation -------------------------------------------------------------
+
+mean(log_compare$RMSE, na.rm = T)
+mean(log_compare$MAPE, na.rm = T)
+
+mean(log_compare$RMSE_2m, na.rm = T)
+mean(log_compare$MAPE_2m, na.rm = T)
+
+mean(log_compare$RMSE_3m, na.rm = T)
+mean(log_compare$MAPE_3m, na.rm = T)
+
+# Filtering ---------------------------------------------------------------
+
+fil_model <- c("A520-64CDMSL", "NOVA5T", "Y6SSL", "Y11CDMSL")
+
+log_compare %>% 
+  filter(HS_model %in% fil_model) -> log_sel
+
+# plot --------------------------------------------------------------------
+
+log_compare %>% 
+  select(HS_brand:HS_subtype, RMSE, RMSE_2m, RMSE_3m) %>% 
+  pivot_longer(RMSE:RMSE_3m, "rmse_group", values_drop_na = T) %>% 
+  filter(value < 50) %>% 
+  ggplot(aes(x = value)) + geom_histogram() + facet_grid(rows = vars(rmse_group))
+
+log_compare %>% 
+  select(HS_brand:HS_subtype, MAPE, MAPE_2m, MAPE_3m) %>% 
+  pivot_longer(MAPE:MAPE_3m, "rmse_group", values_drop_na = T) %>% 
+  filter(value < 100) %>%
+  ggplot(aes(x = value)) + geom_histogram() + facet_grid(rows = vars(rmse_group))
+
