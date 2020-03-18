dcaffbfafc557fb4ae219ae3693a93d01de47011
aeroroach <silencepill@gmail.com>
2020-03-18 13:18:43 +0700

    Fix hs_forecast file error.

--- hs_forecast.sh
+++ hs_forecast.sh
@@ -7,7 +7,7 @@
 #Version 2.1 : Boosting new handset model
 #Version 2.2 : Fix bug for some newly launch model
 #Version 2.3 : Limit training to 3 month and unlock cap max
-#Version 2.4 : Fixed over forecast and set cap max 
+#Version 2.4 : Fixed bug for cap max
 
 I_AM_VERSION="V2.4"
 APP_HOME="/home/tdmdf/HS_forecast"
